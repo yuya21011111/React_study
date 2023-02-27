52aac35ffeffc396be72c409990ca2e1c814f360
YuyaYamazi <kaimomo12345@gmail.com>
2023-02-28 05:06:48 +0900

    React_study2

---
+++ styling/src/App.js
@@ -0,0 +1,38 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import "./Example.css";
+
+const Example = () => {
+    return (
+        <div className="component">
+            <h3>Heloo Component</h3>
+        </div>
+    );
+};
+
+export default Example;
