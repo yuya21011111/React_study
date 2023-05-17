e32b6c53d5f17d7b3f4a6c28e4295c8f78d8a6ca
YuyaYamazi <kaimomo12345@gmail.com>
2023-05-18 04:17:38 +0900

    64_state_array

--- 05_state_and_event/src/064_state_array/start/Example.js
+++ 05_state_and_event/src/064_state_array/start/Example.js
@@ -1,9 +1,17 @@
+import { useState } from "react";
 const Example = () => {
   const numArray = [1, 2, 3, 4, 5];
-
+  const [nums, setNums] = useState(numArray)
+  const shuffle = () => {
+    const newNums = [...nums];
+    const last = newNums.pop();
+    newNums.unshift(last);
+    setNums(newNums);
+  }
   return (
     <>
-      
+      <h1>{nums}</h1>
+      <button onClick={shuffle}>shuffle</button>
     </>
   );
 };
--- 05_state_and_event/src/068_practice_obj_state/start/Example.js
+++ 05_state_and_event/src/068_practice_obj_state/start/Example.js
@@ -3,20 +3,30 @@ import { useState } from 'react';
 const Example = () => {
   const orderObj = { item: 'apple', count: 10 };
   const [order, setOrder] = useState(orderObj);
-  const changeItem = (e) => {};
-  const countUp = () => {};
-  const countDown = () => {};
+  const changeItem = (e) => {
+    setOrder({...order, item: e.target.value})
+  };
+  const countUp = () => {
+    setOrder({...order,count: order.count + 1})
+  };
+  const countDown = () => {
+    setOrder({...order,count: order.count - 1})
+  };
+  const reset = () => {
+    setOrder({count: 10,item: 'apple'})
+  };
   return (
     <div>
       <h3>練習問題</h3>
       <p>
         記述を変更し、完成コードのように「+と-ボタンをクリックするとCountの表示が1ずつ増減する機能」と「input要素に連動してItemの表示が変更される機能」を実装してください。コンポーネントの外側（上部）に変数や関数を準備しているためうまく使ってください。
       </p>
-      <h3>Item:{/* ここにorder.itemを表示してください。*/}</h3>
-      <h3>Count:{/* ここにorder.countを表示してください。*/}</h3>
+      <h3>Item:{order.item}</h3>
+      <h3>Count:{order.count}</h3>
       <input type="text" value={order.item} onChange={changeItem} />
       <button onClick={countUp}>+</button>
       <button onClick={countDown}>-</button>
+      <button onClick={reset}>reset</button>
     </div>
   );
 };
