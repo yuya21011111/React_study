ee704a948d7bd77a41a37d7d4474f0f8eaf0a52b
YuyaYamazi <kaimomo12345@gmail.com>
2023-05-19 04:22:16 +0900

    70_state_and_component

--- 05_state_and_event/src/070_state_and_component/start/Example.js
+++ 05_state_and_event/src/070_state_and_component/start/Example.js
@@ -1,6 +1,22 @@
 import { useState } from "react";
 
 const Example = () => {
+  const [ toggle, setToggle ] = useState(true);
+  const toggle_chack = () => {
+    // setToggle(!toggle)
+    setToggle(prev => !prev)
+  }
+  return (
+    <>
+    <button onClick={toggle_chack}>Toggle</button>
+    {toggle ? <Count key="A" title="A" /> : <div><Count key="B" title="B" /></div> }
+    {/* <Count title="A" />
+    {toggle && <Count title="B" /> } */}
+    </>
+  )
+}
+
+const Count = ({title}) => {
   const [count, setCount] = useState(0);
   const countUp = () => {
     setCount((prevstate) => prevstate + 1);
@@ -10,7 +26,7 @@ const Example = () => {
   };
   return (
     <>
-      <h3>カウント: {count}</h3>
+      <h3>{title}: {count}</h3>
       <button onClick={countUp}>+</button>
       <button onClick={countDown}>-</button>
     </>
