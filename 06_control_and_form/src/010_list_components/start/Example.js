e5408a1ac8c993ee060130dd5725f6859c5c9b8a
YuyaYamazi <kaimomo12345@gmail.com>
2023-05-21 16:57:52 +0900

    10_list_component

--- 06_control_and_form/src/010_list_components/start/Example.js
+++ 06_control_and_form/src/010_list_components/start/Example.js
@@ -2,12 +2,23 @@
 const animals = ["Dog", "Cat", "Rat"];
 
 const Example = () => {
+  const animalsList = [];
+  animals.map(function(val){
+    animalsList.push(<li key={val}>{val}</li>);
+  });
+
+  const helloAnimals = animals.map((animal) => 
+    <li key={animal}>Hello, {animal}</li>
+  );
+
+  console.log(helloAnimals);
 
   return (
     <>
       <h3>配列の操作</h3>
       <ul>
-        
+        {animalsList}
+        {helloAnimals}
       </ul>
     </>
   );
