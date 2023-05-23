7e53dcfb20164dc14b5227123db248b3ea3997a1
YuyaYamazi <kaimomo12345@gmail.com>
2023-05-24 03:44:57 +0900

    60_conditional

--- 06_control_and_form/src/060_conditional_render/start/Example.js
+++ 06_control_and_form/src/060_conditional_render/start/Example.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 const Example = () => {
-  const animals = ["Dog", "Cat", "Rat"];
+  const animals = ["Dog", "Cat",null, "Rat"];
 
   const [filterVal, setFilterVal] = useState("");
 
@@ -15,13 +15,25 @@ const Example = () => {
       <ul>
         {animals
           .filter((animal) => {
-            const isMatch = animal.indexOf(filterVal) !== -1;
-            console.log(animal.indexOf(filterVal));
+            const animalStr = animal ?? "";
+            const isMatch = animalStr.indexOf(filterVal) !== -1;
+          
             return isMatch;
           })
-          .map((animal) => (
-            <li key={animal}>{animal}</li>
-          ))}
+          .map((animal) => {
+            return(
+            <li key={animal}>
+              {animal ?? 'null'}
+              {animal === 'Dog' && '★'}
+            </li>
+            )
+            // return <li key={animal}>{animal === 'Dog' ? animal + '★' : animal}</li>
+            // if(animal === 'Dog') {
+            //   return <li key={animal}>{animal + '★'}</li>
+            // } else {
+            //   return <li key={animal}>{animal}</li>
+            // }
+        })}
       </ul>
     </>
   );
