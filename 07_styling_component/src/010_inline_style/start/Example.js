ec17b92915fb343b3e04d9102e711b643dc2616f
YuyaYamazi <kaimomo12345@gmail.com>
2023-05-31 23:53:17 +0900

    010_inline_style

--- 07_styling_component/src/010_inline_style/start/Example.js
+++ 07_styling_component/src/010_inline_style/start/Example.js
@@ -5,10 +5,22 @@ const Example = () => {
 
     const clickHandler = () => setIsSelected(prev => !prev);
 
+    const style = {
+        width: 120,
+        height: 60,
+        display: "block",
+        fontWeight: "bold",
+        "border-radius": 9999,
+        cursor: "pointer",
+        border: "none",
+        margin: "auto",
+        background: isSelected ? "pink" : ""
+    }
+
     return (
         <>
-            <button onClick={clickHandler}>ボタン</button>
-            <div>{isSelected && "クリックされました。"}</div>
+            <button onClick={clickHandler} style={style}>ボタン</button>
+            <div style={{ textAlign: "center" }}>{isSelected && "クリックされました。"}</div>
         </>
     )
 };
