51e721684bcfbb0dc02e204536f168bef150358b
YuyaYamazi <kaimomo12345@gmail.com>
2023-06-02 04:10:35 +0900

    040_css_in_js

--- 07_styling_component/src/040_css_in_js/start/Example.js
+++ 07_styling_component/src/040_css_in_js/start/Example.js
@@ -1,12 +1,26 @@
 import { useState } from "react";
+import styled from "styled-components";
 
+console.dir(styled);
+const StyledButton = styled.button`
+    margin: auto;
+    border-radius: 9999px;
+    border: none;
+    display: block;
+    width: 120px;
+    height: 60px;
+    font-weight: bold;
+    cursor: pointer;
+    background-color: ${(props) => props.isSelected ? 'Pink' : ''};
+`;
 const Example = () => {
-  const [isSelected, setIsSelected] = useState(false);
+  const [isSelected, setIsSelected] = useState(true);
 
   const clickHandler = () => setIsSelected((prev) => !prev);
 
   return (
     <>
+      <StyledButton isSelected={isSelected} onClick={clickHandler}>ボタン</StyledButton>
       <button className={`btn ${isSelected ? "selected" : ""}`} onClick={clickHandler}>
         ボタン
       </button>
