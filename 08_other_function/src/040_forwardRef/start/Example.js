a34eee8e1e8dbf9e46e6f7ae8c6fc1231e6087f9
YuyaYamazi <kaimomo12345@gmail.com>
2023-06-07 04:18:16 +0900

    40_forwardRef

--- 08_other_function/src/040_forwardRef/start/Example.js
+++ 08_other_function/src/040_forwardRef/start/Example.js
@@ -1,10 +1,14 @@
 import { useRef, forwardRef } from "react";
 
+const Input = forwardRef((props,ref) => {
+  return <input type="text" ref={ref} />
+});
+
 const Example = () => {
   const ref = useRef();
   return (
     <>
-      <input type="text" ref={ref} />
+      <Input ref={ref} />
       <button onClick={() => ref.current.focus()}>
         インプット要素をフォーカスする
       </button>
