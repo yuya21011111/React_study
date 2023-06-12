ff5b03d623b72a14d8a63dc8a9e95d7a77fe1f30
YuyaYamazi <kaimomo12345@gmail.com>
2023-06-13 04:11:39 +0900

    40_react_pure

--- 10_functional_programming/src/040_react_pure_fn/start/Example.js
+++ 10_functional_programming/src/040_react_pure_fn/start/Example.js
@@ -1,7 +1,24 @@
-const Example = () => {
+let val = 0;
 
+const Child = () => {
+  val++;
+  return <div>{val}</div>
+}
+
+const ChildPure = ({ val }) => {
+  return <div>{ val }</div>
+}
+
+const Example = () => {
+let val = 0;
   return (
     <>
+    <Child />
+    <Child />
+    <Child />
+    <ChildPure val={++val} />
+    <ChildPure val={++val} />
+    <ChildPure val={++val} />
     </>
   );
 };
