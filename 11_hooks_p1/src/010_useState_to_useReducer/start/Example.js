e6ccccd9dd7f85697cad98da84a58b550f4836fa
YuyaYamazi <kaimomo12345@gmail.com>
2023-06-16 05:15:01 +0900

    25_practice

--- 11_hooks_p1/src/010_useState_to_useReducer/start/Example.js
+++ 11_hooks_p1/src/010_useState_to_useReducer/start/Example.js
@@ -1,7 +1,51 @@
+import { useReducer,useState } from "react";
 const Example = () => {
-  
+  const [state, setState] = useState(0);
+  const [rstate,dispatch] = useReducer((preve,{ type,step }) => {
+    // if(action === '+') {
+    //   console.log(1,preve)
+    //   return ++preve;
+    // } else {
+    //   console.log(2,preve)
+    //   return --preve;
+    // }
+
+    switch(type){
+      case '+':
+       return preve + step;
+      case '-':
+        return preve - step;
+      default:
+        throw new Error('不明なactionです！');
+    }
+  },0)
+  // const  countUp = () => {
+  //   setState(state + 1)
+  // }
+
+  const  countUp = () => {
+    setState(preve => ++preve)
+  }
+
+  const  rcountUp = () => {
+    dispatch({type: '+',step: 2});
+   }
+
+  const  rcountDown = () => {
+   dispatch({type: '-', step: 3});
+  }
   return (
     <>
+    <div>
+    <h3>{state}</h3>
+    <button onClick={countUp}>+</button>
+    </div>
+
+    <div>
+    <h3>{rstate}</h3>
+    <button onClick={rcountUp}>+</button>
+    <button onClick={rcountDown}>-</button>
+    </div>
     </>
   );
 };
--- 11_hooks_p1/src/025_practice_useReducer/start/Example.js
+++ 11_hooks_p1/src/025_practice_useReducer/start/Example.js
@@ -2,7 +2,26 @@ import { useReducer } from "react";
 
 const CALC_OPTIONS = ["add", "minus", "divide", "multiply"];
 
-const reducer = () => {}
+const reducer = (state,{type,payload}) => {
+  switch (type) {
+    case 'change':{
+      return {...state,[payload.name]: payload.value};
+    }
+    case 'add':
+        return {...state,result: state.a + state.b};
+    case 'minus':{
+        return {...state,result: state.a - state.b};
+    }
+    case 'divide':{
+        return {...state,result: state.a * state.b};
+    }
+    case 'multiply':{
+        return {...state,result: state.a / state.b};
+    }
+    default:
+        throw new Error("operator is invalid");
+  }
+};
 
 const Example = () => {
   const initState = {
@@ -14,18 +33,20 @@ const Example = () => {
   const [state, dispatch] = useReducer(reducer, initState);
 
   const calculate = (e) => {
-    
+   dispatch({type: e.target.value})
   };
 
   const numChangeHandler = (e) => {
-    
+   
+   
+    dispatch({type: 'change',payload: {name: e.target.name, value: parseInt(e.target.value)}});
   }
 
   return (
     <>
     <h3>練習問題</h3>
     <p>useReducerを使って完成コードと同じ機能を作成してください。</p>
-      {/* <div>
+      <div>
         a:
         <input
           type="number"
@@ -44,8 +65,9 @@ const Example = () => {
         />
       </div>
       <select value={state.type} onChange={calculate}>
+      {CALC_OPTIONS.map((type) => <option key={type} value={type}>{type}</option>)}
       </select>
-      <h1>結果：{state.result}</h1> */}
+      <h1>結果：{state.result}</h1>
     </>
   );
 };
