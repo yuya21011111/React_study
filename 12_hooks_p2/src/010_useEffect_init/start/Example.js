fc393faa0802eb968da970b14b68c9296eff09e2
YuyaYamazi <kaimomo12345@gmail.com>
2023-07-02 16:46:35 +0900

    10_useEffect

--- 12_hooks_p2/src/010_useEffect_init/start/Example.js
+++ 12_hooks_p2/src/010_useEffect_init/start/Example.js
@@ -1,5 +1,26 @@
+import {useState,useEffect} from "react";
 const Example = () => {
-  return <></>;
+  const [time,setTime] = useState(0);
+
+  useEffect(() => {
+    console.log("useEffectOk")
+    window.setInterval(() => {
+      setTime(prev => prev + 1);
+    }, 1000);
+  },[])
+
+  
+
+  // window.setInterval(() => {
+  //   console.log("useEffectNot")
+  //   setTime(prev => prev + 1);
+  // }, 1000);
+  return <>
+  <h3>
+    <time>{time}</time>
+    <span>秒経過</span>
+  </h3>
+  </>
 };
 
 export default Example;
