5452c88ae967957da453e74cac7c6a7802c0ae5b
YuyaYamazi <kaimomo12345@gmail.com>
2023-07-03 17:57:58 +0900

    30_useEffect_clear

--- 12_hooks_p2/src/030_useEffect_cleanup/start/Example.js
+++ 12_hooks_p2/src/030_useEffect_cleanup/start/Example.js
@@ -1,18 +1,38 @@
 import { useEffect, useState } from "react";
+const Example =() => {
+  const [isDisp,setIsDisp] = useState(true);
+  
+  return (
+    <>
+    {isDisp && <Timer />}
+    <button onClick={() => setIsDisp(prev => !prev) }>トグル</button>
+    </>
+  )
+}
 
-const Example = () => {
+const Timer = () => {
   const [time, setTime] = useState(0);
 
   useEffect(() => {
-    console.log('useEffect is called');
-    window.setInterval(() => {
+    console.log('init');
+    let intervalId = null;
+    intervalId = window.setInterval(() => {
+      console.log('interval called');
       setTime(prev => prev + 1);
     }, 1000);
+    return () => {
+      window.clearInterval(intervalId);
+      // console.log('end')
+    }
   }, [])
   
   useEffect(() => {
+    console.log('updated')
     document.title = 'counter:' + time;
-    window.localStorage.setItem('time-key-end', time)
+    window.localStorage.setItem('time-key-end', time);
+    return () => {
+      console.log('updatedEnd')
+    }
   }, [time]);
 
   return (
