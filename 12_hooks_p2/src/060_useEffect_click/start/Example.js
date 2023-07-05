affa7a0b051ba0c117f38fa17348940bada06894
YuyaYamazi <kaimomo12345@gmail.com>
2023-07-06 04:47:02 +0900

    60_useEffect_click

--- 12_hooks_p2/src/060_useEffect_click/start/Example.js
+++ 12_hooks_p2/src/060_useEffect_click/start/Example.js
@@ -6,26 +6,29 @@ const Example = () => {
   return (
     <>
       {isDisp && <Timer/>}
-      <button onClick={() => setIsDisp(prev => !prev)}>トグル</button>
+      <button onClick={() => setIsDisp(prev => !prev)}>{isDisp ? '非表示' : '表示'}</button>
     </>
   )
 }
 
 const Timer = () => {
   const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
 
   useEffect(() => {
     // console.log('init');
     let intervalId = null;
-    intervalId = window.setInterval(() => {
-      // console.log('interval running');
-      setTime(prev => prev + 1);
-    }, 1000);
+    if(isRunning) {
+      intervalId = window.setInterval(() => {
+        // console.log('interval running');
+        setTime(prev => prev + 1);
+      }, 1000);
+    }
     return () => {
       window.clearInterval(intervalId)
       // console.log('end');
     }
-  }, [])
+  }, [isRunning])
   
   useEffect(() => {
     // console.log('updated');
@@ -46,11 +49,25 @@ const Timer = () => {
     }
   }, [])
 
+  const toggle = () => {
+    setIsRunning(prev => !prev)
+  }
+  const reset = () => {
+    setTime(0);
+    setIsRunning(false);
+
+  }
   return (
+    <>
     <h3>
       <time>{time}</time>
       <span>秒経過</span>
     </h3>
+    <div>
+      <button onClick={toggle}>{isRunning ?  '一時停止':'スタート'}</button>
+      <button onClick={reset}>リセット</button>
+    </div>
+    </>
     );
 };
 
